4004b78958bfced204ff5cfe79a018a472eb7ce7
hrg921 <hrg921@gmail.com>
2018-07-20 21:13:38 +0900

    fix generate json
    not refactored when refactor database connection

--- generate_json.py
+++ generate_json.py
@@ -1,10 +1,12 @@
 import json
-from connect_database import conn
+from connect_database import connect_database
 
 
 def generate_json():
+    conn = connect_database()
     with conn.cursor() as cursor:
         cursor.execute('SELECT * FROM articles')
         result = cursor.fetchall()
     with open('data.json', 'w') as outfile:
             json.dump(result, outfile)
+    conn.close()
